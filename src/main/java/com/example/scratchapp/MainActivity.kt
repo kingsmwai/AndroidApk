2d7df2994eb173804a7d1051d081ffbbedbfee29
mwaikings <mwaikings4@gmail.com>
2021-04-12 21:14:27 +0300

    first commit

---
+++ src/main/java/com/example/scratchapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.scratchapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var TextView:TextView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+      setContentView(R.layout.activity_main)
+
+
+
+    }
+}
